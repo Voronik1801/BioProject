51bd7ae7b138bc5023c37c578c6cbe8735e5ce46
voronik1801 <divi1801@icloud.com>
2021-02-04 12:01:22 +0700

    woking csv reading & considered differrent variants for reading

---
+++ Bio/Reader.py
@@ -0,0 +1,4 @@
+#как еще один вариант хранения данных.
+# from pandas import read_csv
+# dataset_RS = read_csv('File/data.csv', ',', header=None)
+# print(len(dataset_RS))
--- Bio/cvsReader.py
+++ Bio/cvsReader.py
@@ -1,16 +1,24 @@
 import csv
+def PrintMatrix (matrix):
+    for i in range(len(dataForAnalys)):
+        for j in range(len(dataForAnalys[i])):
+            print(dataForAnalys[i][j], " ", end="")
+        print()
 
-with open('File/TB_data_dictionary_2021-01-28.csv', 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+with open('File/data.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    dataForAnalys=list(csv_reader) #рабочий и самый подходящий вариант для дальнейшего анализа
+    PrintMatrix(dataForAnalys)
 
 
-    with open('File/new_csv.csv','w') as new_file:
-        fieldnames = ['variable_name', 'code_list', 'definition']
 
-        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+#попытки
+    # for line in csv_reader:
+    #     lengthLine = (len(line))  # как по мне не самый оптимизированный вариант нахождения длины строки
+    #     lengthRow += 1
+    # print(lengthLine)
+    # print(lengthRow)
+    # print(line)
+    # data = list(line)
+    # print(data)
 
-        csv_writer.writeheader()
-
-        for line in csv_reader:
-            del line['dataset']
-            csv_writer.writerow(line)

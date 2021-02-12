4b14a9d3c468c549a57fd38a80cb3fe6ea936c8c
voronik1801 <divi1801@icloud.com>
2021-02-12 18:14:02 +0700

    adding pls and right saving of data, pls on nipals

--- Bio/cvsReader.py
+++ Bio/cvsReader.py
@@ -1,24 +1,48 @@
 import csv
-def PrintMatrix (matrix):
-    for i in range(len(dataForAnalys)):
-        for j in range(len(dataForAnalys[i])):
-            print(dataForAnalys[i][j], " ", end="")
+from sklearn.cross_decomposition import PLSRegression
+
+
+# Print matrix
+def PrintMatrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print(matrix[i][j], " ", end="")
         print()
 
-with open('File/data.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    dataForAnalys=list(csv_reader) #рабочий и самый подходящий вариант для дальнейшего анализа
-    PrintMatrix(dataForAnalys)
+
+# Saving to matrix X in list for Analysis, reading in csv
+def ImportToX(matrix):
+    for i in range(1, len(matrix)):
+        for j in range(len(matrix[i]) - 1):
+            X[i - 1][j] = int(matrix[i][j])
 
 
+# Saving to vector Y in list for Analysis, reading in csv
+def ImportToY(matrix):
+    for i in range(1, len(matrix)):
+        for j in range(len(matrix[i]) - 1, len(matrix[i])):
+            Y[i - 1] = matrix[i][j]
 
-#попытки
-    # for line in csv_reader:
-    #     lengthLine = (len(line))  # как по мне не самый оптимизированный вариант нахождения длины строки
-    #     lengthRow += 1
-    # print(lengthLine)
-    # print(lengthRow)
-    # print(line)
-    # data = list(line)
-    # print(data)
 
+# Open csv and save
+with open('File/data2.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    dataForAnalys = list(csv_reader)  # рабочий и самый подходящий вариант для дальнейшего анализа
+
+    # Defined X and Y for next PLS methosS
+    X = [0] * (len(dataForAnalys) - 1)
+    for i in range(len(dataForAnalys) - 1):
+        X[i] = [0] * (len(dataForAnalys[i]) - 1)
+        Y = [0] * (len(dataForAnalys[i]) - 1)
+    # Saving data for analysis in main structure for pls
+    ImportToX(dataForAnalys)
+    ImportToY(dataForAnalys)
+
+plsNipals = PLSRegression(n_components=1) #defined pls, default stand nipals
+plsNipals.fit(X, Y) #Fit model to data.
+PLSRegression() #create PLS
+predNipals = plsNipals.predict(X) #create answer PLS
+print(predNipals)
+
+#this method dont work, why? i all do like in documentation
+#plsSVD = PLSRegression(n_components=1, algorithm="svd")

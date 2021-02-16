55c6a216df37afe12a04bb2f9b120dfaf8dc7c96
Voronik <55249362+Voronik1801@users.noreply.github.com>
2021-02-16 12:12:41 +0700

    Add files via upload

--- Bio/cvsReader.py
+++ Bio/cvsReader.py
@@ -2,6 +2,7 @@ import csv
 from sklearn.cross_decomposition import PLSRegression
 import matplotlib.pyplot as plt
 
+
 # Print matrix
 def PrintMatrix(matrix):
     for i in range(len(matrix)):
@@ -13,15 +14,14 @@ def PrintMatrix(matrix):
 # Saving to matrix X in list for Analysis, reading in csv
 def ImportToX(matrix):
     for i in range(1, len(matrix)):
-        for j in range(len(matrix[i]) - 1):
-            X[i - 1][j] = int(matrix[i][j])
+        for j in range(0, len(matrix[i]) - 1):
+            X[i - 1][j] = float(matrix[i][j])
 
 
 # Saving to vector Y in list for Analysis, reading in csv
 def ImportToY(matrix):
     for i in range(1, len(matrix)):
-        for j in range(len(matrix[i]) - 1, len(matrix[i])):
-            Y[i - 1] = int(matrix[i][j])
+        Y[i - 1] = float(matrix[i][len(matrix[i])-1])
 
 
 # Open csv and save
@@ -29,21 +29,26 @@ with open('File/data2.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
     dataForAnalys = list(csv_reader)  # рабочий и самый подходящий вариант для дальнейшего анализа
 
-    # Defined X and Y for next PLS methosS
-    X = [0] * (len(dataForAnalys) - 1)
-    for i in range(len(dataForAnalys) - 1):
-        X[i] = [0] * (len(dataForAnalys[i]) - 1)
-        Y = [0] * (len(dataForAnalys[i]) - 1)
-    # Saving data for analysis in main structure for pls
-    ImportToX(dataForAnalys)
-    ImportToY(dataForAnalys)
+# Defined X and Y for next PLS methosS
+X = [0] * (len(dataForAnalys) - 1)
+for i in range(0, len(dataForAnalys) - 1):
+    X[i] = [0] * (len(dataForAnalys[i]) - 1)
+Y = [0] * (len(dataForAnalys[1]) - 1)
+# Saving data for analysis in main structure for pls
+ImportToX(dataForAnalys)
+ImportToY(dataForAnalys)
+print(X)
+print(Y)
 
-plsNipals = PLSRegression(n_components=1) #defined pls, default stand nipals
-plsNipals.fit(X, Y) #Fit model to data.
-PLSRegression() #create PLS
-predNipals = plsNipals.predict(X) #create answer PLS
+plsNipals = PLSRegression(n_components=1)  # defined pls, default stand nipals
+plsNipals.fit(X, Y)  # Fit model to data.
+PLSRegression()  # create PLS
+predNipals = plsNipals.predict(X)  # create answer PLS
 print(predNipals)
+param = plsNipals.get_params(True)
+print(param)
 plt.plot(predNipals)
 plt.show()
-#this method dont work, why? i all do like in documentation
-#plsSVD = PLSRegression(n_components=1, algorithm="svd")
+
+# this method dont work, why? i all do like in documentation
+# plsSVD = PLSRegression(n_components=1, algorithm="svd")

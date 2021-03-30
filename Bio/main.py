dd0c1474a0e124f528528da98c86c2010ce9a402
voronik1801 <divi1801@icloud.com>
2021-03-30 22:35:53 +0700

    Add cross validation, find biiiiiiig error in input data

--- Bio/main.py
+++ Bio/main.py
@@ -5,6 +5,16 @@ import numpy as np
 import pandas as pd
 
 
+def CrossValidation(X, Y, comp):
+    resultCV = np.zeros(X.shape[0])
+    for i in range(n):
+        beginX = X
+        predictX = X[i]
+        beginX = np.delete(beginX, [i], 0)
+        regression = PLS1Regression(X,Y,comp)
+        predictY = regression.Predict(predictX)
+        resultCV[i] = predictY
+    return resultCV
 
 
 class PLS1Regression:
@@ -13,27 +23,36 @@ class PLS1Regression:
         if _X.shape[1] < _components:
             print("error")
         else:
-            self.X = _X
-            self.Y = _Y
-            self.components = _components
-            self.B, self.B0 = self.PLS1()
+            self.X = _X # matrix of predictors
+            self.Y = _Y # vector of real data
+            self.components = _components # number of latent variable
+            self.n = _X.shape[0] # number of testing people
+            self.p = _X.shape[1] # number of properties
+            self.B, self.B0 = self.PLS1() # regression coef and regression const
+
+    def centerscale(self,scale=True):
+        self.X -= np.amin(self.X, axis=(0, 1))
+        self.X /= np.amax(self.X, axis=(0, 1))
+
+
+
     def PLS1(self):
         #init X0 & y0
+        self.centerscale()
         Xk = self.X
         y = self.Y
 
         # n - size of testee
         # p - size of properties
-        n = Xk.shape[0]
-        p = Xk.shape[1]
+
 
         # W - help matrix of weights
         # P, q - Load matrix
-        W = np.zeros((p, self.components))
-        P = np.zeros((p, self.components))
+        W = np.zeros((self.p, self.components))
+        P = np.zeros((self.p, self.components))
         b = np.zeros(self.components)
-        t = np.zeros((n, self.components))  # x_scores
-        p_loading = np.zeros((p, self.components)) # x_loading
+        t = np.zeros((self.n, self.components))  # x_scores
+        p_loading = np.zeros((self.p, self.components)) # x_loading
 
         W[:,0] = X.T.dot(y)/np.linalg.norm(X.T.dot(y))
 
@@ -64,11 +83,7 @@ class PLS1Regression:
         return B, B0
 
     def Predict(self, X):
-
-        # for i in range (len(B)):
-        #     print(B[i])
         return X.dot(self.B) + self.B0
-
 
 class Utils:
 
@@ -114,6 +129,27 @@ class Utils:
         plt.show()
         plt.show()
 
+    def PrintError(self,X,Y):
+        #Print err
+        for k in range (15,31):
+            # dataCV = CrossValidation(X,Y,k)
+            #
+            # plsNipals = PLSRegression(n_components=k)  # defined pls, default stand nipals
+            # plsNipals.fit(X, Y)  # Fit model to data.
+            # predNipals = plsNipals.predict(X)  # create answer PLS
+
+            regress = PLS1Regression(X,Y, k)
+            other = regress.Predict(X)
+            err = np.zeros(n)
+            scal = 0
+            for j in range(0, len(Y)):
+                # err[j] = (dataCV[j]-Y[j])**2
+                # err[j] = (predNipals[j]-Y[j])**2
+                err[j] = (other[j]-Y[j])**2
+                scal+=err[j]
+            print(np.sqrt(scal),"\t")
+
+
 
 utils = Utils()
 # Open csv and save
@@ -124,6 +160,8 @@ with open('File/table_aMD_th_0.80_00.csv', 'r') as csv_file:
 n = len(dataForAnalys)- 1
 p = len(dataForAnalys[0]) - 1
 
+components = 30
+
 X = np.zeros((n, p))
 Y = np.zeros(n)
 
@@ -131,33 +169,17 @@ Y = np.zeros(n)
 utils.ImportToX(dataForAnalys)
 utils.ImportToY(dataForAnalys)
 
-plsNipals = PLSRegression(n_components=10)  # defined pls, default stand nipals
-plsNipals.fit(X, Y)  # Fit model to data.
-predNipals = plsNipals.predict(X)  # create answer PLS
-X = np.array([[0., 0., 1.], [1.,0.,0.], [2.,2.,2.], [2.,5.,4.]])
-Y = np.array([0.1, 0.9, 6.2, 11.9])
+# plsNipals = PLSRegression(n_components=components)  # defined pls, default stand nipals
+# plsNipals.fit(X, Y)  # Fit model to data.
+# predNipals = plsNipals.predict(X)  # create answer PLS
 
-Xpred = np.array([2.,5.,4.])
-#plt.plot(Y)
-regress = PLS1Regression(X,Y, 3)
-other = regress.Predict(Xpred)
-print(other)
-#plt.plot(other)
-# plt.show()
-#utils.CratePlot(Y,predNipals,other)
+# regress = PLS1Regression(X,Y, components)
+# other = regress.Predict(X)
+dataCV = np.zeros(n)
+#dataCV = CrossValidation(X,Y,components)
 
-# for i in range (len(other)):
-#     print(other[i])
+# utils.CratePlot(Y,predNipals,other)
+utils.PrintError(X,Y)
 
-#Print err
-# for k in range (1,11):
-#     other = regression.Predict(k, X, Y)
-#     for i in range (len(Y)):
-#         err = [0] * len(Y)
-#         scal = 0
-#         for j in range(0, len(Y)):
-#             err[j] = (other[j]-Y[j])**2
-#             scal+=err[j]
-#     print(np.sqrt(scal),"\t")
 
 

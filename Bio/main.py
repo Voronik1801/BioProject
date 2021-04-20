267603e7a0114ba01134cfadfb198cbaa5567ef7
voronik1801 <divi1801@icloud.com>
2021-04-20 07:14:48 +0700

    robust method in process

--- Bio/main.py
+++ Bio/main.py
@@ -2,22 +2,30 @@ import csv
 import matplotlib.pyplot as plt
 from sklearn.cross_decomposition import PLSRegression
 import numpy as np
-import pandas as pd
-
-
 def CrossValidation(X, Y, comp):
+
     resultCV = np.zeros(X.shape[0])
     for i in range(n):
         beginX = X
         predictX = X[i]
+        predictYtrue = Y[i]
+        beginY = Y
         beginX = np.delete(beginX, [i], 0)
-        regression = PLS1Regression(X,Y,comp)
-        predictY = regression.Predict(predictX)
-        resultCV[i] = predictY
+        beginY = np.delete(beginY, [i], 0)
+        # regression = PLSRegression(n_components=comp)
+        # regression.fit(beginX,beginY)
+        # predictYpred=regression.predict(predictX.reshape(1,-1))
+
+        regression = PLS1Regression(beginX,beginY,comp)
+        predictYpred = regression.Predict(predictX.reshape(1,-1))
+
+        #print(predictYtrue-predictYpred)
+        resultCV[i] = predictYpred
     return resultCV
 
 
 class PLS1Regression:
+    c = 0.1
     def __init__(self, _X, _Y, _components):
         #Кол-во компонени должно быть меньше количества признаков, иначе нельзя раскладывать
         if _X.shape[1] < _components:
@@ -30,15 +38,50 @@ class PLS1Regression:
             self.p = _X.shape[1] # number of properties
             self.B, self.B0 = self.PLS1() # regression coef and regression const
 
+    def Hiuber(self, x1):
+        # if abs(z) < c:
+        #     return 1.0/2.0 * (z**2)
+        # else:
+        #     return c * (abs(z)-1.0/2.0 * c**2)
+        return x1[0] ** 2 +x1[1] ** 2
+
     def centerscale(self,scale=True):
         self.X -= np.amin(self.X, axis=(0, 1))
         self.X /= np.amax(self.X, axis=(0, 1))
 
-
+    def HukaJivsa(self, x0, h, eps, a):
+        S = np.zeros(len(x0))
+        xnext = np.zeros(len(x0))
+        h0 = h
+        while (h0 >= eps):
+            x = x0
+            xtest = x
+            fpred = self.Hiuber(xtest)
+            for k in range (len(x0)):
+                #exploring search
+                xtest[k] = xtest[k] + h
+                ftest = self.Hiuber(xtest)
+                if (fpred > ftest):
+                    x = xtest
+                    fpred = ftest
+                else:
+                    xtest[k] = xtest[k] - 2 * h
+                    ftest = self.Hiuber(xtest)
+                    if (fpred > ftest):
+                        x = xtest
+                        fpred = ftest
+                    xtest[k] = xtest[k] + h
+                S[k] = x[k] - x0[k]
+                #search for a sample
+                xnext[k] = x[k] + (x[k] -xpred[k])
+                xpred[k] = x[k]
+                x[k] = xnext[k]
+                h0 = h0/a
+        return x
 
     def PLS1(self):
         #init X0 & y0
-        self.centerscale()
+        #self.centerscale()
         Xk = self.X
         y = self.Y
 
@@ -54,7 +97,7 @@ class PLS1Regression:
         t = np.zeros((self.n, self.components))  # x_scores
         p_loading = np.zeros((self.p, self.components)) # x_loading
 
-        W[:,0] = X.T.dot(y)/np.linalg.norm(X.T.dot(y))
+        W[:,0] = Xk.T.dot(y)/np.linalg.norm(Xk.T.dot(y))
 
         for k in range (0,self.components):
             t[:,k] = np.dot(Xk, W[:,k]) #x_scores
@@ -131,9 +174,9 @@ class Utils:
 
     def PrintError(self,X,Y):
         #Print err
-        for k in range (15,31):
-            # dataCV = CrossValidation(X,Y,k)
-            #
+        for k in range (1,31):
+            #dataCV = CrossValidation(X,Y,k)
+
             # plsNipals = PLSRegression(n_components=k)  # defined pls, default stand nipals
             # plsNipals.fit(X, Y)  # Fit model to data.
             # predNipals = plsNipals.predict(X)  # create answer PLS
@@ -143,8 +186,10 @@ class Utils:
             err = np.zeros(n)
             scal = 0
             for j in range(0, len(Y)):
-                # err[j] = (dataCV[j]-Y[j])**2
-                # err[j] = (predNipals[j]-Y[j])**2
+                #err[j] = (dataCV[j]-Y[j])**2
+
+                #err[j] = (predNipals[j]-Y[j])**2
+
                 err[j] = (other[j]-Y[j])**2
                 scal+=err[j]
             print(np.sqrt(scal),"\t")
@@ -160,7 +205,7 @@ with open('File/table_aMD_th_0.80_00.csv', 'r') as csv_file:
 n = len(dataForAnalys)- 1
 p = len(dataForAnalys[0]) - 1
 
-components = 30
+c = 1
 
 X = np.zeros((n, p))
 Y = np.zeros(n)
@@ -169,17 +214,24 @@ Y = np.zeros(n)
 utils.ImportToX(dataForAnalys)
 utils.ImportToY(dataForAnalys)
 
-# plsNipals = PLSRegression(n_components=components)  # defined pls, default stand nipals
+# plsNipals = PLSRegression(n_components=c)  # defined pls, default stand nipals
 # plsNipals.fit(X, Y)  # Fit model to data.
 # predNipals = plsNipals.predict(X)  # create answer PLS
 
-# regress = PLS1Regression(X,Y, components)
-# other = regress.Predict(X)
-dataCV = np.zeros(n)
-#dataCV = CrossValidation(X,Y,components)
+regress = PLS1Regression(X,Y, c)
+other = regress.Predict(X)
 
+# dataCV = np.zeros(n)
+# dataCV = CrossValidation(X,Y,c)
+# scal = 0
+# err = np.zeros(n)
+# for j in range(0, len(Y)):
+#     err[j] = (dataCV[j] - Y[j]) ** 2
+#     #err[j] = (predNipals[j]-Y[j])**2
+#     #err[j] = (other[j]-Y[j])**2
+#     scal += err[j]
+# print(np.sqrt(scal), "\t")
 # utils.CratePlot(Y,predNipals,other)
-utils.PrintError(X,Y)
-
-
-
+#utils.PrintError(X,Y)
+x1 = np.array([1.0, 1.0])
+print(regress.HukaJivsa(x1, 1, 0.001, 2))

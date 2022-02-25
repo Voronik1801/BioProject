82317c83bc59d3c40ecaf7a4ac8752d5be3e48e1
Jopchiki <divi1801@icloud.com>
2022-02-25 11:55:25 +0700

    feat: delete unusefull file and string

--- Bio/main.py
+++ Bio/main.py
@@ -32,35 +32,6 @@ def random_value():
     value = random.randint(0, 71)
     return value
 
-def main_pls():
-    # Open csv and save
-    with open('BioProject/Bio/pls_value.csv', 'r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=';')
-        dataForAnalys = list(csv_reader)
-
-    # Defined X and Y for next PLS methos
-    n = len(dataForAnalys) - 1
-    p = len(dataForAnalys[0]) - 1
-
-    X = np.zeros((n, p))
-    Y = np.zeros(n)
-    utils = ls_ut(X, Y)
-
-    # Saving data for analysis in main structure for pls
-    utils.ImportToX(dataForAnalys)
-    utils.ImportToY(dataForAnalys)
-    err = utils.ErrorLib(X, Y)
-    
-    # print(LA.det(np.dot(X.T, X)))
-    # for i in err:
-        # print(err[i])
-    # components = [5]
-    # for i in components:
-        # regress = PLS1Regression(X, Y, i, 'classic')
-        # y_oz = regress.Predict(X)
-    #     R = r2_score(Y, y_oz)
-    #     print(R)
-
 
 def draw_graph(G):
     options = {
@@ -102,12 +73,6 @@ class GraphStructure():
 
         self.donor = [donor[0].split('-')[0] for donor in donor_akceptor.values]
         self.akceptor = [akceptor[0].split('-')[1] for akceptor in donor_akceptor.values]
-
-        self.donor_ost = [d.split('@')[0] for d in self.donor]
-        self.akceptor_ost = [a.split('@')[0] for a in self.akceptor]
-
-        self.donor_ost_wo = [d.split('.')[0] for d in self.donor]
-        self.akceptor_ost_wo = [a.split('.')[0] for a in self.akceptor]
         
     def load_values_in_graph(self, donor, akceptor, weights):
         Graph = nx.Graph()
@@ -121,81 +86,8 @@ class GraphStructure():
         for i in range(n):
             self.Graphs_full.append(self.load_values_in_graph(self.donor, self.akceptor, self.weights[i]))
         for i in range(n):
-            adj_matrix = nx.adjacency_matrix(self.Graphs_full[i])
-            print(adj_matrix)
-            # self.Graphs_full[i] = self.uniq_subgraphs(self.Graphs_full[i])
-        # draw_graph(self.Graphs_full[5])
-        # draw_graph(self.Graphs_full[0])
-
-    # def creat_full_value_graph(self):
-    #     n = len(self.weights)
-    #     for i in range(n):
-    #         self.Graphs_full.append(self.load_values_in_graph(self.donor, self.akceptor, self.weights[i]))
-    #     for i in range(n):
-    #         conn = self.anslysis_graph(self.Graphs_full[i])
-    #         self.connection_nodes.append(conn)
-    #     self.filter()
-    #     for i in range(len(self.Graphs_full)):
-    #         self.new.append(self.new_graph(self.Graphs_full[i], i) )
-    #     self.Graphs_full = self.new
-    #     # draw_graph(self.Graphs_full[0])
-
-    # def filter(self):
-    #     i = 0
-    #     n = len(self.weights)
-    #     while i < n:
-    #         if self.connection_nodes[i] == None:
-    #             self.connection_nodes.remove(self.connection_nodes[i])
-    #             self.Graphs_full.remove(self.Graphs_full[i])
-    #             self.surv_time.remove(self.surv_time[i])
-    #             n -= 1
-    #             continue
-    #         i += 1
-
-    # def new_graph(self, G, i):
-    #     Graph = nx.Graph()
-    #     connection = self.connection_nodes[i]
-    #     for edge in connection:
-    #         Graph.add_edge(edge[0], edge[1])
-    #     # draw_graph(Graph)
-    #     return Graph
-
-    # def anslysis_graph(self, G):
-    #     conn = []
-    #     for n in G.nodes:
-    #         a = len(list(nx.dfs_edges(G, n)))
-    #         if a == 6:
-    #             dfs = list(nx.dfs_edges(G, n))
-    #             conn = dfs
-    #             return conn
-    #     return None                   
-
-    # def filter(self):
-    #     i = 0
-    #     n = len(self.weights)
-    #     while i < n:
-    #         if self.connection_nodes[i] == None:
-    #             self.connection_nodes.remove(self.connection_nodes[i])
-    #             self.Graphs_full.remove(self.Graphs_full[i])
-    #             self.surv_time.remove(self.surv_time[i])
-    #             n -= 1
-    #             continue
-    #         i += 1                  
-    
-    def uniq_subgraphs(self, G):
-        exsisting_subgraph = []
-        new_Graph = nx.Graph()
-        for n in G.nodes:
-            nodes_subgraph = list(nx.dfs_edges(G, n))
-            sub = nx.classes.function.edge_subgraph(G, nodes_subgraph)
-            subgraph = nx.Graph(sub)
-            if subgraph not in exsisting_subgraph:
-                isomorph = [nx.is_isomorphic(subgraph, exs_subgr) for exs_subgr in exsisting_subgraph]
-                if not True in isomorph:
-                    for u, v, w in sub.edges(data=True):
-                        new_Graph.add_edge(u, v, weght=w['weight'])
-                    exsisting_subgraph.append(subgraph)
-        return new_Graph
+            self.Graphs_full[i] = self.uniq_subgraphs(self.Graphs_full[i])
+        draw_graph(self.Graphs_full[0])
 
     # def uniq_subgraphs(self, G):
     #     exsisting_subgraph = []
@@ -206,43 +98,36 @@ class GraphStructure():
     #         subgraph = nx.Graph(sub)
     #         if subgraph not in exsisting_subgraph:
     #             isomorph = [nx.is_isomorphic(subgraph, exs_subgr) for exs_subgr in exsisting_subgraph]
-    #             if not True in isomorph:
+    #             if not True in isomorph and len(nodes_subgraph) == 3:
     #                 for u, v, w in sub.edges(data=True):
     #                     new_Graph.add_edge(u, v, weght=w['weight'])
+    #                     break
     #                 exsisting_subgraph.append(subgraph)
-    #                 break
     #     return new_Graph
+    def anslysis_graph(self, G):
+        k = 0
+        for n in G.nodes:
+            a = len(list(nx.dfs_postorder_nodes(G, n)))
+            if a == 3:
+                conn = list(nx.dfs_postorder_nodes(G, n))
+                break
+        return conn
 
-    # def uniq_subgraphs(self, G):
-    #     exsisting_subgraph = []
-    #     new_Graph = nx.Graph()
-    #     conn = self.anslysis_graph(G)
-    #     for node in conn:
-    #         a = list(G.edges)
-    #         for n in a:
-    #             if node in n:
-    #                 n = list(n)
-    #                 new_Graph.add_edge(n[0], n[1])
-    #     return new_Graph
+    def uniq_subgraphs(self, G):
+        exsisting_subgraph = []
+        new_Graph = nx.Graph()
+        conn = self.anslysis_graph(G)
+        for node in conn:
+            a = list(G.edges(data=True))
+            for n in a:
+                if node in n:
+                    n = list(n)
+                    new_Graph.add_edge(n[0], n[1], weight=n[2]['weight'])
+        return new_Graph
 
     def calculate_prop(self, G):
         property = []
-        # if G == None: 
-        #     return [0, 0 , 0] 
-        # nodes = G.nodes
-        # # degree_sequence = [d for n, d in G.degree()]
-        # # property=degree_sequence
-        # for n in nodes:
-        #     property.append(G.degree[n])
-        #     property.append(len(list(nx.dfs_postorder_nodes(G, n))))
-        #     cycles = nx.cycle_basis(G, n)
-        #     property.append(len(cycles))
-        #     cycle = sorted([len(c) for c in nx.cycle_basis(G, n)])
-        #     if cycle != []:
-        #         property.append(max(cycle))
-        #     else:
-        #         property.append(0)
-        property = nx.adj_matrix(G)
+        det_adj = LA.det(nx.adj_matrix(G))
         return property
 
     
@@ -352,77 +237,27 @@ def error(y, y_oz):
 
 def main_graph():
     structure = GraphStructure()
-    structure.calculate_main_values('BioProject/Bio/graph_value.csv')
+    structure.calculate_main_values('D:\Diplom\BioProject\Bio\graph_value.csv')
     structure.property_kol = 200
     X, Y = structure.full_graph_calc() #1
-    i = 0
-    while True:
-        if i == structure.property_kol:
-            break
-        else:
-            df[f'degree{i}'] = X[:, i]
-            i += 1
-        if i == structure.property_kol:
-            break
-        else:
-            df[f'len dfs{i}'] = X[:, i]
-            i += 1
-        if i == structure.property_kol:
-            break
-        else:
-            df[f'len cycle{i}'] = X[:, i]
-            i += 1
-        if i == structure.property_kol:
-            break
-        else:
-            df[f'max cycle{i}'] = X[:, i]
-            i += 1
-    # print(df)
     X = uniq(X)
     write_x(X)
     ones = np.ones(len(structure.Graphs_full))
-    for i in range(structure.property_kol):
-        for j in range(len(X[0])):
-            try:
-                if (X[:, j] == df[f'degree{i}'].values).all():
-                    res_df[f'degree{i}'] = df[f'degree{i}'].values
-                    break
-            except:
-                pass
-            try:
-                if (X[:, j] == df[f'len dfs{i}'].values).all():
-                    res_df[f'len dfs{i}'] = df[f'len dfs{i}'].values
-                    break
-            except:
-                pass
-            try:
-                if (X[:, j] == df[f'len cycle{i}'].values).all():
-                    res_df[f'len cycle{i}'] = df[f'len cycle{i}'].values
-                    break
-            except:
-                pass
-            try:
-                if (X[:, j] == df[f'max cycle{i}'].values).all():
-                    res_df[f'max cycle{i}'] = df[f'max cycle{i}'].values
-                    break
-            except:
-                pass
-    print(res_df.columns)
     X = centr_norm(X)
     X = np.hstack((X, np.atleast_2d(ones).T))
     write_x(X)
-    X = np.linalg.qr(X)[0]
+    # X = np.linalg.qr(X)[0]
     print(LA.det(np.dot(X.T, X)))
-    ols_prediction(X, Y)
+    # ols_prediction(X, Y)
     # components = [2, 3, 4]
     # # print(LA.eig(np.dot(X.T, X)))
     # for k in components:
     #     y_oz, R = pls_prediction(X, Y, k)
     #     print(R)
     #     print('---')
-    utils = ls_ut(X, Y)
-    cv = cross_validation(X, Y)
-    utils.CreateTwoPlot(Y, cv)
-    print(error(Y, cv))
+    # utils = ls_ut(X, Y)
+    # cv = cross_validation(X, Y)
+    # utils.CreateTwoPlot(Y, cv)
+    # print(error(Y, cv))
 
 main_graph()
